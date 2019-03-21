c11bc4f908a15a75fe7fd906aadc71bb9991eecc
aditihaiman <ahaiman00@stuy.edu>
2019-03-21 09:49:16 -0400

    remove first and constructors

--- MyDeque.java
+++ MyDeque.java
@@ -3,21 +3,23 @@ public class MyDeque<E> {
   private E[] data;
   private int size, start, end;
 
-  public MyDeque() {
-    
+@SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
   }
 
+@SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
-
-
+    data = (E[])new Object[initialCapacity];
   }
 
   public int size(){
-
+    return size;
   }
 
   public String toString(){
-
+    String output = "";
+    return output;
   }
 
   public void addFirst(E element){
@@ -28,20 +30,30 @@ public class MyDeque<E> {
 
   }
 
-  public E removeFirst(E element){
+  public E removeFirst(){
+    E temp = data[start];
+    if(start==end) data[start] = null;
+    else if(start==size-1){
+      data[start] = null;
+      start = 0;
+    }
+    else {
+      data[start] = null;
+      start++;
+    }
+    return temp;
+  }
+
+  public E removeLast(){
 
   }
 
-  public E removeLast(E element){
-
+  public E getFirst(){
+    return data[start];
   }
 
-  public E getFirst(E element){
-
-  }
-
-  public E getLast(E element){
-
+  public E getLast(){
+    return data[end];
   }
 
 
