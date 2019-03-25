ba2cff4383ea8e936aa8c2296cea5c61307f3f7c
aditihaiman <ahaiman00@stuy.edu>
2019-03-25 09:58:28 -0400

    initial

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      
+
+
+
+
+    }
+}
